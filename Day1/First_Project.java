df798a8e785eae11cefa9e2bbd72e2df29a08f63
arafaelnwehy10 <arafa.elnwehy10@gmail.com>
2026-08-25 03:36:33 +0300

    adding day 1

---
+++ Day1/First_Project.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class First_Project {
+
+    static void main() {
+        System.out.println("hello arafa");
+
+    }
+}
---
+++ Day1/datatypes.java
@@ -0,0 +1,43 @@
+package Day1;
+
+public class datatypes {
+    static void main() {
+        int cats = 3;
+        System.out.println("number of cat = "+cats);
+        char x='E';
+        String name ="MESSI";
+        System.out.println("the goat is "+name);
+        int a= 10;
+        long y=a;
+        System.out.println(y);
+        int q=1,w=2;
+        System.out.println((q+w));
+        System.out.println((q-w));
+        System.out.println((q*w));
+        System.out.println((q/w));
+        System.out.println(q==w);
+        System.out.println(q!=w);
+        System.out.println(q>=w);
+        System.out.println(q<=w);
+        System.out.println(q>w);
+        System.out.println(q<w);
+int r=10;
+int t= r++;
+        System.out.println(r);
+        System.out.println(t);
+        int o=10;
+        o+=5;
+        o-=5;
+        o*=5;
+        o%=5;
+        System.out.println(o);
+        int age=13;
+        String status=age>18?"eligable":"not eligable";
+        System.out.println(status);
+
+
+
+
+
+    }
+}
