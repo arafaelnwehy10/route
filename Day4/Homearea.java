f4513dc2b079a6eb7e2af4d5f78b8a3819f8eae7
arafaelnwehy10 <arafa.elnwehy10@gmail.com>
2026-08-28 21:51:16 +0300

    adding day 4

---
+++ Day4/Homearea.java
@@ -0,0 +1,20 @@
+package Day4;
+
+public class Homearea {
+    static void main() {
+        Rectangle r1=new Rectangle();
+
+        r1.setLenght(20);
+        r1.setWidht(10);
+        double area_1=r1.calculate_area();
+       System.out.println("area_1  = "+area_1);
+       Rectangle r2=new Rectangle(15,45);
+//       r2.setLenght(20);
+//       r2.setWidht(10);
+       double area_2=r2.calculate_area();
+       System.out.println("area_2  = "+area_2);
+       System.out.println("total area = "+(area_1+area_2));
+
+   }
+
+}
---
+++ Day4/Rectangle.java
@@ -0,0 +1,34 @@
+package Day4;
+
+ public   class Rectangle {
+   private double lenght;
+    private double widht;
+    public Rectangle(){
+        System.out.println("hi mohamed");
+    }
+     public Rectangle(double lenght,double widht){
+         System.out.println("hi arafa");
+//         lenght=8;
+//         widht=8;
+         setWidht(widht);
+         setLenght(lenght);
+     }
+   public void setLenght(double lenght){
+        this.lenght=lenght;
+    }
+     public void setWidht(double widht){
+         this.widht=widht;
+     }
+     public double getLenght(){
+       return lenght;
+     }
+     public double getWidht(){
+         return widht;
+     }
+  public   double calculate_area(){
+        return lenght*widht;
+    }
+    public double calculate_perimeter(){
+        return lenght*2+widht*2;
+    }
+}
